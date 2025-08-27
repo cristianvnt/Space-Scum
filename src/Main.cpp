dfe908dafc36ca54b6da5987ba5ec86bcc14c7c7
Mematoru <mematoru@gmail.com>
2025-08-27 10:29:38 +0300

    Added bullets

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,8 +1,13 @@
 #include "raylib.h"
 
-constexpr int SCREEN_WIDTH = 800;
-constexpr int SCREEN_HEIGHT = 600;
-constexpr float RADIUS = 20.f;
+#include <vector>
+#include <iostream>
+
+constexpr int SCREEN_WIDTH = 1000;
+constexpr int SCREEN_HEIGHT = 700;
+
+// bullet stuff
+constexpr Vector2 bBody = { 10.f, 10.f };
 
 constexpr int FPS = 60;
 
@@ -13,7 +18,22 @@ static void ProcessInput(float dt);
 static void UpdateAndDraw(float dt);
 static void CheckBounds();
 
-Vector2 player = { 0, 0 };
+static void SpawnBullet();
+static void UpdateBullets(float dt);
+
+struct Player
+{
+	Rectangle body{};
+	float speed{};
+};
+
+struct Bullet
+{
+	Rectangle body{};
+	float speed;
+	bool active;
+	Color color;
+};
 
 int main()
 {
@@ -33,15 +53,20 @@ int main()
 	CloseWindow();
 }
 
+Player player;
+std::vector<Bullet> bullets;
+
 void InitGame()
 {
-	player = { (float)SCREEN_WIDTH / 2.f, (float)SCREEN_HEIGHT / 1.5f };
+	player.body = { (float)SCREEN_WIDTH / 2.f, (float)SCREEN_HEIGHT / 1.5f, 50.f, 70.f };
+	player.speed = 200;
 }
 
 void UpdateGame(float dt)
 {
 	CheckBounds();
 	ProcessInput(dt);
+	UpdateBullets(dt);
 }
 
 void DrawStuff()
@@ -49,7 +74,11 @@ void DrawStuff()
 	BeginDrawing();
 
 	ClearBackground(RAYWHITE);
-	DrawCircleV(player, RADIUS, BLUE);
+	DrawRectangleRec(player.body, BLUE);
+	
+	for (size_t i = 0; i < bullets.size(); ++i)
+		DrawRectangleRec(bullets[i].body, bullets[i].color);
+
 	DrawFPS(10, 10);
 
 	EndDrawing();
@@ -58,29 +87,50 @@ void DrawStuff()
 void ProcessInput(float dt)
 {
 	if (IsKeyDown(KEY_W))
-		player.y -= 100.f * dt;
+		player.body.y -= player.speed * dt;
 	if (IsKeyDown(KEY_S))
-		player.y += 100.f * dt;
+		player.body.y += player.speed * dt;
 	if (IsKeyDown(KEY_A))
-		player.x -= 100.f * dt;
+		player.body.x -= player.speed * dt;
 	if (IsKeyDown(KEY_D))
-		player.x += 100.f * dt;
+		player.body.x += player.speed * dt;
+
+	if (IsKeyPressed(KEY_SPACE))
+		SpawnBullet();
 }
 
 void CheckBounds()
 {
-	if (player.y < (float)GetScreenHeight() / 2.f)
-		player.y = (float)GetScreenHeight() / 2.f;
-	if (player.y > (float)GetScreenHeight() - RADIUS)
-		player.y = (float)GetScreenHeight() - RADIUS;
-	if (player.x > (float)GetScreenWidth() - RADIUS)
-		player.x = (float)GetScreenWidth() - RADIUS;
-	if (player.x < RADIUS)
-		player.x = RADIUS;
+	if ((player.body.x + player.body.width) >= GetScreenWidth())
+		player.body.x = GetScreenWidth() - player.body.width;
+	if (player.body.x <= 0)
+		player.body.x = 0;
+	if ((player.body.y + player.body.height) >= GetScreenHeight())
+		player.body.y = GetScreenHeight() - player.body.height;
+	if ((player.body.y) <= GetScreenHeight() / 2.f)
+		player.body.y = GetScreenHeight() / 2.f;
 }
 
 void UpdateAndDraw(float dt)
 {
 	UpdateGame(dt);
 	DrawStuff();
+}
+
+void SpawnBullet()
+{
+	bullets.push_back(Bullet{ {player.body.x, player.body.y, bBody.x, bBody.y}, 100.f, true, RED });
+}
+
+void UpdateBullets(float dt)
+{
+	for (size_t i = 0; i < bullets.size(); ++i)
+	{
+		if (!bullets[i].active)
+			continue;
+
+		bullets[i].body.y -= bullets[i].speed * dt;
+		bullets.erase(std::remove_if(bullets.begin(), bullets.end(), [](const Bullet& b) {
+			return b.body.y < -b.body.height; }), bullets.end());
+	}
 }
